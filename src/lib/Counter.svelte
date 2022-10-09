7bd9c25d7975b70a8b525fa4bfea99c7e58a0074
Ali Alaa <hello@alialaa.com>
2022-10-09 12:51:50 +0200

    reactive gotcha 1

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -1,18 +1,24 @@
 <script>
-	let count = 0;
-	// let string = `You have clicked ${count} times.`;
-	$: string = `You have clicked ${count} times.`;
+	let count1 = 0;
+	let count2 = 0;
 
-	$: if (count > 5) {
-		console.log(`The count is ${count}`);
+	function getTotal() {
+		return count1 + count2;
 	}
+	// Not going to work
+	$: string = `The count total is ${getTotal()}.`;
 
-	function increment() {
-		count += 1;
+	function increment1() {
+		count1 += 1;
+	}
+	function increment2() {
+		count2 += 1;
 	}
 </script>
 
-<button on:click={increment}>Clicks {count}</button>
+<button on:click={increment1}>Clicks {count1}</button>
+<button on:click={increment2}>Clicks {count2}</button>
+
 <h3>{string}</h3>
 
 <style>
