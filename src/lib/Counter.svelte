f277d1e88c7ac2afa236dc8556b5fdb5855d2ed4
Ali Alaa <hello@alialaa.com>
2022-10-09 13:00:16 +0200

    reactive gotcha 2

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -2,22 +2,19 @@
 	let count1 = 0;
 	let count2 = 0;
 
-	function getTotal() {
-		return count1 + count2;
+	function setCount2(x) {
+		count2 = x;
 	}
-	// Not going to work
-	$: string = `The count total is ${getTotal()}.`;
+
+	$: setCount2(count1);
+	$: string = `Count2 is ${count2}.`;
 
 	function increment1() {
 		count1 += 1;
 	}
-	function increment2() {
-		count2 += 1;
-	}
 </script>
 
 <button on:click={increment1}>Clicks {count1}</button>
-<button on:click={increment2}>Clicks {count2}</button>
 
 <h3>{string}</h3>
 
