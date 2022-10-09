3a0ce86c9b83b973385395e69963436c0c314665
Ali Alaa <hello@alialaa.com>
2022-10-09 11:55:32 +0200

    First component

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+	let name = 'Svelte';
+	let src = 'http://picsum.com/200/200';
+	let string = 'Some <strong>string</strong> with html.';
+</script>
+
+<img {src} alt="Some text {name}" />
+<h1>Hello {name.toLowerCase()}!</h1>
+<p>{@html string}</p>
+
+<style>
+	h1 {
+		color: red;
+	}
+</style>
