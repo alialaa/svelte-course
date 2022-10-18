5d905d5c9bfadd2810f66f80d622f04c2edf6abb
Ali Alaa <hello@alialaa.com>
2022-10-18 10:48:02 +0200

    Todos init and each loop

--- src/App.svelte
+++ src/App.svelte
@@ -1,27 +1,27 @@
 <script>
-	import Button from './lib/Button.svelte';
-	import FaAngellist from 'svelte-icons/fa/FaAngellist.svelte';
-	import FaAllergies from 'svelte-icons/fa/FaAllergies.svelte';
+	import TodoList from './lib/TodoList.svelte';
+	import { v4 as uuid } from 'uuid';
+
+	const todos = [
+		{
+			id: uuid(),
+			title: 'Todo 1',
+			completed: true
+		},
+		{
+			id: uuid(),
+			title: 'Todo 2',
+			completed: false
+		},
+		{
+			id: uuid(),
+			title: 'Todo 3',
+			completed: true
+		}
+	];
 </script>
 
-<Button
-	on:click|once={(event) => {
-		alert(true);
-	}}
-	let:isLeftHovered
-	size="small"
-	shadow
-	disabled
->
-	<div style:width="20px" slot="leftContent">
-		{#if isLeftHovered}
-			<FaAngellist />
-		{:else}
-			<FaAllergies />
-		{/if}
-	</div>
-	Button Text
-</Button>
+<TodoList {todos} />
 
 <style>
 </style>
---
+++ src/lib/TodoList.svelte
@@ -0,0 +1,10 @@
+<script>
+	export let todos = [];
+</script>
+
+<ul>
+	{#each todos as { id, title }, index (id)}
+		{@const number = index + 1}
+		<li>{number}- {title}</li>
+	{/each}
+</ul>
