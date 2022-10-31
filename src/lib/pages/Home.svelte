fead73cb70b400bccadd2779723919738f032b9c
Ali Alaa <hello@alialaa.com>
2022-10-31 13:14:25 +0200

    autosubs and binding

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,15 +1,7 @@
 <script>
-  import { onDestroy } from 'svelte';
   import settings from '../stores/settings';
-  let colorScheme;
-  const unsubscribe = settings.subscribe((settings) => {
-    console.log('home subscribe');
-    colorScheme = settings.colorScheme;
-  });
-
-  onDestroy(unsubscribe);
 </script>
 
 <h2>Home</h2>
 
-{colorScheme}
+{$settings.colorScheme}
--- src/lib/pages/Settings.svelte
+++ src/lib/pages/Settings.svelte
@@ -1,22 +1,18 @@
 <script>
-  import { onDestroy } from 'svelte';
   import settings from '../stores/settings';
-  let colorScheme;
-  const unsubscribe = settings.subscribe((settings) => {
-    console.log('settings subscribe');
-    colorScheme = settings.colorScheme;
-  });
-  $: {
-    settings.update((prevSettings) => ({ ...prevSettings, colorScheme }));
-  }
-  onDestroy(unsubscribe);
 </script>
 
 <h2>Settings</h2>
 
+<button
+  on:click={() => {
+    $settings.colorScheme = $settings.colorScheme === 'dark' ? 'light' : 'dark';
+  }}>Toggle Color Scheme</button
+>
+{$settings.colorScheme}
 <label>
-  <input type="radio" bind:group={colorScheme} name="colorScheme" value="dark" /> Dark
+  <input type="radio" bind:group={$settings.colorScheme} name="colorScheme" value="dark" /> Dark
 </label>
 <label>
-  <input type="radio" bind:group={colorScheme} name="colorScheme" value="light" /> Light
+  <input type="radio" bind:group={$settings.colorScheme} name="colorScheme" value="light" /> Light
 </label>
