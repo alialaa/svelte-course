cc66da4e4274f83eb54736c5d1613c8b0f8ae563
Ali Alaa <hello@alialaa.com>
2022-10-31 19:41:10 +0200

    iterpolate colors

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,24 +1,43 @@
 <script>
   import { tweened } from 'svelte/motion';
   import { cubicOut } from 'svelte/easing';
+  import { interpolateRgb } from 'd3-interpolate';
 
-  const size = tweened(
-    { width: 100, height: 100 },
+  const boxProps = tweened(
+    { width: 100, height: 100, color: 'purple' },
     {
       duration: 500,
-      easing: cubicOut
+      easing: cubicOut,
+      interpolate: (a, b) => (t) => {
+        const deltaWidth = b.width - a.width;
+        const deltaHeight = b.height - a.height;
+        const color = interpolateRgb(a.color, b.color)(t);
+        return {
+          width: a.width + deltaWidth * t,
+          height: a.height + deltaHeight * t,
+          color
+        };
+      }
     }
   );
 </script>
 
 <button
   on:click={async () => {
-    await size.set({
-      width: Math.random() * 500,
-      height: Math.random() * 500
-    });
+    await boxProps.set(
+      {
+        width: Math.random() * 500,
+        height: Math.random() * 500,
+        color: `#${Math.floor(Math.random() * 16777215).toString(16)}`
+      },
+      {
+        duration: 4000
+      }
+    );
     console.log('done');
-  }}>Random Size</button
+  }}>Random Box</button
 >
 
-<div style="width: {$size.width}px; height: {$size.height}px; background-color: purple;" />
+<div
+  style="width: {$boxProps.width}px; height: {$boxProps.height}px; background-color: {$boxProps.color};"
+/>
