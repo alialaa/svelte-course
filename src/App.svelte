f8bb16e882f6f4d67fff8473077a5933f370871e
Ali Alaa <hello@alialaa.com>
2022-10-31 12:23:28 +0200

    mini router

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,36 @@
 <script>
-  import tippy from './lib/actions/tippy';
+  import { onMount } from 'svelte';
+  import Home from './lib/pages/Home.svelte';
+  import Settings from './lib/pages/Settings.svelte';
 
-  let content = 'Hello';
+  let page;
+
+  function onRouteChange() {
+    const path = window.location.hash.slice(1);
+    if (path === '/') {
+      page = 'home';
+    } else if (path === '/settings') {
+      page = 'settings';
+    } else {
+      window.location.hash = '/';
+    }
+  }
+
+  onMount(onRouteChange);
 </script>
 
-<input bind:value={content} />
+<svelte:window on:hashchange={onRouteChange} />
 
-<button use:tippy={{ content, placement: 'right', theme: 'light' }}>Button</button>
+<nav>
+  <a href="#/">Home</a>
+  <a href="#/settings">Setting</a>
+</nav>
+
+{#if page === 'home'}
+  <Home />
+{:else if page === 'settings'}
+  <Settings />
+{/if}
 
 <style>
 </style>
