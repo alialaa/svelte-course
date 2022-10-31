e8ca30abffa91bd3c2a4e393ff459053657e6c7b
Ali Alaa <hello@alialaa.com>
2022-10-31 13:57:09 +0200

    store callback

--- src/lib/stores/settings.js
+++ src/lib/stores/settings.js
@@ -1,9 +1,23 @@
 import { writable } from 'svelte/store';
 
-const settings = writable({
-  colorScheme: 'dark',
-  language: 'en',
-  fontSize: 12
-});
+const settings = writable(
+  {
+    colorScheme: 'dark',
+    language: 'en',
+    fontSize: 12
+  },
+  (set) => {
+    const timer = setTimeout(() => {
+      set({
+        colorScheme: 'light',
+        language: 'en',
+        fontSize: 12
+      });
+    }, 1000);
+    return () => {
+      clearTimeout(timer);
+    };
+  }
+);
 
 export default settings;
