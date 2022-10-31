dcc84f5f4c4a6d74a9d1f5e0e44d21238b8bb0d6
Ali Alaa <hello@alialaa.com>
2022-10-31 19:18:16 +0200

    tweened store

--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,14 +1,24 @@
 <script>
-  import settings from '../stores/settings';
-  import location from '../stores/location';
+  import { tweened } from 'svelte/motion';
+  import { cubicOut } from 'svelte/easing';
+
+  const size = tweened(
+    { width: 100, height: 100 },
+    {
+      duration: 500,
+      easing: cubicOut
+    }
+  );
 </script>
 
-<h2>Home</h2>
+<button
+  on:click={async () => {
+    await size.set({
+      width: Math.random() * 500,
+      height: Math.random() * 500
+    });
+    console.log('done');
+  }}>Random Size</button
+>
 
-{#if $location && !$location.error}
-  {$location.latitude}, {$location.longitude}
-{/if}
-{#if $location && $location.error}
-  {$location.error.message}
-{/if}
-{$settings.colorScheme}
+<div style="width: {$size.width}px; height: {$size.height}px; background-color: purple;" />
