9eda47efd5ef92db560c15b2bc675f225bbb4544
Ali Alaa <hello@alialaa.com>
2022-11-06 11:54:12 +0200

    form and field

---
+++ src/lib/Form/Field.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { v4 as uuid } from 'uuid';
+
+  export let name;
+  export let type = 'text';
+  export let label = undefined;
+  export let validate = undefined;
+
+  const id = uuid();
+</script>
+
+<div class="field">
+  {#if label}
+    <label for={id}>{label}</label>
+  {/if}
+  <input {id} {name} {type} placeholder={label} />
+</div>
+
+<style>
+  div.field {
+    margin-bottom: 10px;
+  }
+  label {
+    display: block;
+    font-size: 14px;
+    margin-bottom: 5px;
+  }
+  input {
+    height: 30px;
+    width: 250px;
+    max-width: 100%;
+  }
+</style>
--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,57 +1,11 @@
 <script>
   import Button from '../Button.svelte';
-
-  let values = { username: '', email: '', password: '' };
-  let errors = {};
-  let isSubmitting = false;
-
-  function validate() {
-    const errors = {};
-    if (!values.username) {
-      errors.username = 'The Username is Required.';
-    }
-    if (!values.email) {
-      errors.email = 'The Email is Required.';
-    }
-    if (!values.password) {
-      errors.password = 'The Password is Required.';
-    }
-    if (values.email && !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(values.email)) {
-      errors.email = 'The Email is Invalid.';
-    }
-    return errors;
-  }
+  import { Field, Form } from '../Form';
 </script>
 
-<form
-  on:submit|preventDefault={() => {
-    errors = validate();
-    if (Object.keys(errors).length === 0) {
-      isSubmitting = true;
-      setTimeout(() => {
-        isSubmitting = false;
-      }, 1000);
-    }
-  }}
->
-  <label for="username">Username:</label><br />
-  <input id="username" name="username" type="text" bind:value={values.username} /><br />
-  {#if errors.username}<p>{errors.username}</p>{/if}
-
-  <label for="email">Email:</label><br />
-  <input id="email" name="email" type="email" bind:value={values.email} /><br />
-  {#if errors.email}<p>{errors.email}</p>{/if}
-
-  <label for="password">Password:</label><br />
-  <input id="password" name="password" type="password" bind:value={values.password} /><br />
-  {#if errors.password}<p>{errors.password}</p>{/if}
-
-  <Button type="submit" disabled={isSubmitting}>Submit</Button>
-</form>
-
 <Form on:submit={() => {}}>
-  <Field name="username" type="text" validate={() => {}} />
-  <Field name="email" type="email" validate={() => {}} />
-  <Field name="password" type="password" validate={() => {}} />
+  <Field label="Username" name="username" type="text" validate={() => {}} />
+  <Field label="Email" name="email" type="email" validate={() => {}} />
+  <Field label="password" name="password" type="password" validate={() => {}} />
   <Button type="submit">Submit</Button>
 </Form>
