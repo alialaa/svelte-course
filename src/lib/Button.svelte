8e8965d059b7adedc1ef9a512bc8487552688a38
Ali Alaa <hello@alialaa.com>
2022-11-12 12:22:36 +0200

    module context buttons count

--- src/lib/Button.svelte
+++ src/lib/Button.svelte
@@ -1,66 +1,83 @@
-<script>
-	export let size = 'small';
-	export let shadow = false;
-	export let bgColor = 'inherit';
-	export let textColor = 'inherit';
+<script context="module">
+  let totalButtons = 0;
 
-	let isLeftHovered;
+  export function getTotalButtons() {
+    return totalButtons;
+  }
 </script>
 
+<script>
+  import { onDestroy } from 'svelte';
+
+  export let size = 'small';
+  export let shadow = false;
+  export let bgColor = 'inherit';
+  export let textColor = 'inherit';
+
+  let isLeftHovered;
+
+  totalButtons += 1;
+
+  onDestroy(() => {
+    totalButtons -= 1;
+  });
+</script>
+
+{totalButtons}
 <button
-	on:click
-	style:--buttonBgColor={bgColor}
-	style:--buttonTextColor={textColor}
-	class:size-lg={size === 'large'}
-	class:size-sm={size === 'small'}
-	class:has-left={$$slots.leftContent}
-	class:shadow
-	{...$$restProps}
+  on:click
+  style:--buttonBgColor={bgColor}
+  style:--buttonTextColor={textColor}
+  class:size-lg={size === 'large'}
+  class:size-sm={size === 'small'}
+  class:has-left={$$slots.leftContent}
+  class:shadow
+  {...$$restProps}
 >
-	{#if $$slots.leftContent}
-		<div
-			class="left-content"
-			on:mouseenter={() => (isLeftHovered = true)}
-			on:mouseleave={() => (isLeftHovered = false)}
-		>
-			<slot name="leftContent" />
-		</div>
-	{/if}
-	<slot {isLeftHovered}>Fallback</slot>
+  {#if $$slots.leftContent}
+    <div
+      class="left-content"
+      on:mouseenter={() => (isLeftHovered = true)}
+      on:mouseleave={() => (isLeftHovered = false)}
+    >
+      <slot name="leftContent" />
+    </div>
+  {/if}
+  <slot {isLeftHovered}>Fallback</slot>
 </button>
 
 <style lang="scss">
-	button {
-		display: flex;
-		align-items: center;
-		border: none;
-		background-color: var(--buttonBgColor);
-		color: var(--buttonTextColor);
-		font-weight: bold;
-		border-radius: 5px;
-		cursor: pointer;
-		.left-content {
-			margin-right: 10px;
-		}
-		&:disabled {
-			opacity: 0.6;
-			cursor: not-allowed;
-		}
-		&:hover {
-			background-image: linear-gradient(rgba(0, 0, 0, 0.4) 0 0);
-		}
-		&:active {
-			background-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
-		}
-		&.size-sm {
-			padding: 15px 20px;
-		}
-		&.size-lg {
-			padding: 20px 25px;
-			font-size: 20px;
-		}
-		&.shadow {
-			box-shadow: 0 0 10px rgba(1, 1, 1, 0.3);
-		}
-	}
+  button {
+    display: flex;
+    align-items: center;
+    border: none;
+    background-color: var(--buttonBgColor);
+    color: var(--buttonTextColor);
+    font-weight: bold;
+    border-radius: 5px;
+    cursor: pointer;
+    .left-content {
+      margin-right: 10px;
+    }
+    &:disabled {
+      opacity: 0.6;
+      cursor: not-allowed;
+    }
+    &:hover {
+      background-image: linear-gradient(rgba(0, 0, 0, 0.4) 0 0);
+    }
+    &:active {
+      background-image: linear-gradient(rgba(255, 255, 255, 0.1) 0 0);
+    }
+    &.size-sm {
+      padding: 15px 20px;
+    }
+    &.size-lg {
+      padding: 20px 25px;
+      font-size: 20px;
+    }
+    &.shadow {
+      box-shadow: 0 0 10px rgba(1, 1, 1, 0.3);
+    }
+  }
 </style>
--- src/lib/pages/Home.svelte
+++ src/lib/pages/Home.svelte
@@ -1,53 +1,20 @@
 <script>
-  import { Stage, Layer, Rect } from '../konva';
-  let showCanvas = true;
-  let x = 20;
-  let width = 300;
-
-  let rect1, rect2;
+  import Button, { getTotalButtons } from '../Button.svelte';
+  let showButton = false;
+  let total;
 </script>
 
-<input type="checkbox" bind:checked={showCanvas} />
-<input type="range" bind:value={x} min={0} max={300} />
-<input type="range" bind:value={width} min={0} max={1000} />
+<input type="checkbox" bind:checked={showButton} />
 
+<Button>Button</Button>
+<Button>Button</Button>
+{#if showButton}
+  <Button>Button</Button>
+{/if}
+
+{total}
 <button
   on:click={() => {
-    console.log(rect1.rect.getAttrs());
-    console.log(rect2.rect.getAttrs());
-    console.log(rect2.rect.getStage());
-    rect2.rect.x(0);
-  }}>Get Rect Info</button
+    total = getTotalButtons();
+  }}>Update buttons count</button
 >
-
-<Stage {width} height={400}>
-  <Layer
-    draggable
-    on:click={() => {
-      console.log('click');
-    }}
-    on:dragmove={(e) => {
-      console.log(e.detail);
-    }}
-  >
-    {#if showCanvas}
-      <Rect bind:this={rect1} {x} y={20} fill="blue" width={200} height={100} />
-    {/if}
-    <Rect x={50} y={50} fill="purple" width={100} height={100} />
-  </Layer>
-
-  <Layer {x}>
-    <Rect
-      on:click={() => console.log('green clicked')}
-      on:mousedown={() => console.log('green mousedown')}
-      bind:this={rect2}
-      x={100}
-      y={80}
-      fill="green"
-      width={100}
-      height={100}
-      stroke="white"
-      strokeWidth={4}
-    />
-  </Layer>
-</Stage>
